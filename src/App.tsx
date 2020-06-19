60e4ff5cf33739b1aff53293aaab78048e3ad4ee
Jacques Blom <39128232+jacquesnf@users.noreply.github.com>
2020-06-19 14:45:49 +0200

    Unused

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import {Heading} from './components/Heading'
 import {Input} from './components/Input'
 import {Stats} from './components/Stats'
